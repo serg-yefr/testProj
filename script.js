ffe558c3d94670bcc6923c3b7bbc9962935b60e7
Sergei Yefremov <55360348+serg-yefr@users.noreply.github.com>
2024-08-27 01:44:42 +0300

    v005

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('click-button').addEventListener('click', function(event
     const now = Date.now();
 
     // Проверяем, прошло ли достаточно времени с последнего клика (200 мс = 5 кликов в секунду)
-    if (now - lastClickTime < 200) {
+    if (now - lastClickTime < 100) {
         return; // Игнорируем клик, если прошло менее 200 мс
     }
 
